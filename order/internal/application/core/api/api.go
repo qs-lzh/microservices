767918943b0660f1e1c65c830f7d6246d1e0d78e
ZiHao Liu <145265650+qs-liuzh@users.noreply.github.com>
2025-10-12 00:06:27 +0800

    finish 5.3.3

--- order/internal/application/core/api/api.go
+++ order/internal/application/core/api/api.go
@@ -1,13 +1,17 @@
 package api
 
 import (
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/qs-lzh/microservices/order/internal/application/core/domain"
 	"github.com/qs-lzh/microservices/order/internal/ports"
 )
 
 type Application struct {
 	db      ports.DBPort
-	payment payment.Adapter
+	payment ports.PaymentPort
 }
 
 func NewApplication(db ports.DBPort, payment ports.PaymentPort) *Application {
@@ -23,8 +27,15 @@ func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
 	}
 	paymentErr := a.payment.Charge(&order)
 	if paymentErr != nil {
-		return domain.Order{}, paymentErr
+		fieldErr := &errdetails.BadRequest_FieldViolation{
+			Field:       "payment",
+			Description: paymentErr.Error(),
+		}
+		badReq := &errdetails.BadRequest{}
+		badReq.FieldViolations = append(badReq.FieldViolations, fieldErr)
+		orderStatus := status.New(codes.InvalidArgument, "order creation failed")
+		statusWithDetails, _ := orderStatus.WithDetails(badReq)
+		return domain.Order{}, statusWithDetails.Err()
 	}
-
 	return order, nil
 }
